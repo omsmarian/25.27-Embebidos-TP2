0e0b79d29377fbc822d0bea5d314f426cd918695
omsmarian <omsmarian@gmail.com>
2024-10-09 03:29:05 -0300

    arrancando ls irqn

--- source/I2C.c
+++ source/I2C.c
@@ -28,11 +28,37 @@
 //Mux
 #define PORT_mAlt5 5
 
+
+//I2C Register Macros
+#define I2C_WRITE_DATA(data)    	(I2C_arr[module]->D = data)
+#define I2C_ADDRESS_MASK			    ((I2C_Objects[module].slave_address & I2C_D_DATA_MASK)<<1)
+#define I2C_SET_NACK	         	  (I2C_array[module]->C1 |= I2C_C1_TXAK_MASK)
+#define I2C_CLEAR_NACK      	 	  (I2C_array[module]->C1 &= ~I2C_C1_TXAK_MASK)
+#define I2C_START_SIGNAL        	(I2C_array[module]->C1 |= I2C_C1_MST_MASK)
+#define I2C_STOP_SIGNAL          	(I2C_array[module]->C1 &= ~I2C_C1_MST_MASK)
+#define I2C_REPEAT_START_SIGNAL 	(I2C_array[module]->C1 |= I2C_C1_RSTA_MASK)
+#define I2C_READ_DATA           	(I2C_array[module]->D)
+#define I2C_STATUS_BYTE           (I2C_array[module]->S)
+#define I2C_GET_TX_MODE				    (I2C_array[module]->C1 & I2C_C1_TX_MASK)
+#define I2C_GET_IRQ_FLAG        	(I2C_array[module]->S & I2C_S_IICIF_MASK)
+#define I2C_CLEAR_IRQ_FLAG      	(I2C_array[module]->S |= I2C_S_IICIF_MASK)
+#define I2C_GET_RX_ACK				    (I2C_array[module]->S & I2C_S_RXAK_MASK)    // 0 = ACK received, 1 = no ACK received
+#define I2C_SET_RX_MODE         	(I2C_array[module]->C1 &= ~I2C_C1_TX_MASK)
+#define I2C_SET_TX_MODE         	(I2C_array[module]->C1 |= I2C_C1_TX_MASK)
+#define I2C_SET_NACK	         	  (I2C_array[module]->C1 |= I2C_C1_TXAK_MASK)
+#define I2C_CLEAR_NACK      	 	  (I2C_array[module]->C1 &= ~I2C_C1_TXAK_MASK)
+
+
+
 /*******************************************************************************
  * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
  ******************************************************************************/
 
-
+// Declaring the baud rate configuration structure
+typedef struct {
+  uint8_t mul; 	    // Multiplier Factor, register value
+  uint8_t icr;	    // ClockRate, register value
+} BaudRate_t;
 
 
 /*******************************************************************************
@@ -60,6 +86,8 @@ static BaudRate_t SetBaudRate (uint32_t desiredBaudRate);
 static I2C_Type* const I2C_arr[] = I2C_BASE_PTRS;           // Array of pointers to I2C modules
 static PORT_Type* const Port_arr[] = PORT_BASE_PTRS;        // Array of pointers to PORT modules
 static IRQn_Type const I2C_IRQn[] = I2C_IRQS;               // Array of IRQs for I2C modules
+static I2C_Object_t I2C_Objects[I2C_COUNT];                 // Array of I2C objects
+
 
 static uint32_t ICR2SCLDivider[] = {
 	20, 22, 24, 26, 28, 30, 34, 40, 28, 32, 36, 40, 44, 48, 56,
@@ -69,11 +97,7 @@ static uint32_t ICR2SCLDivider[] = {
 	1280, 1536, 1920, 1280, 1536, 1792, 2048, 2304, 2560, 3072, 3840
 };
 
-// Declaring the baud rate configuration structure
-typedef struct {
-  uint8_t mul; 	// Multiplier Factor, register value
-  uint8_t icr;	// ClockRate, register value
-} BaudRate_t;
+
 
 /*******************************************************************************
  *******************************************************************************
@@ -94,8 +118,13 @@ void I2C_Init(I2C_Module_t module)
 
 
   // Port configuration
-  SIM->SCG5 |= SIM_SCG5_PORTA(HIGH) | SIM_SCG5_PORTB(HIGH) | 
-              SIM_SCG5_PORTC(HIGH) | SIM_SCG5_PORTD(HIGH) | SIM_SCG5_PORTE(HIGH);
+  if(module == I2C0_M)
+    SIM->SCGC5 |= SIM_SCGC5_PORTE(HIGH);
+  else if(module == I2C1_M)
+    SIM->SCGC5 |= SIM_SCGC5_PORTE(HIGH);
+  else if(module == I2C2_M)
+    SIM->SCGC5 |= SIM_SCGC5_PORTA(HIGH);
+
 
   I2C_Type* I2C = I2C_arr[module];
 
@@ -129,9 +158,27 @@ void I2C_Init(I2C_Module_t module)
 }
 
 
-int32_t I2C_Trasmit()
+I2C_Status_t I2C_Transmit(I2C_Module_t module, uint8_t * sequence_arr, size_t sequence_size, 
+                    I2C_Address_t slave_address, I2C_Address_t reg_address, I2C_Mode_t mode);
 {
-  
+  if(sequence_arr != NULL && sequence_size)
+    {
+      I2C_Objects[module].slave_address = slave_address;
+      I2C_Objects[module].reg_address = reg_address;
+      I2C_Objects[module].sequence_arr = sequence_arr;
+      I2C_Objects[module].sequence_size = sequence_size;
+      I2C_Objects[module].index = 0;
+      I2C_Objects[module].mode = mode;
+    }
+
+
+  // 1 es R, 0 es W
+  I2C_SET_TX_MODE;
+  I2C_START_SIGNAL;
+  I2C_WRITE_DATA(I2C_ADDRESS_MASK);   // always start in write mode
+  I2C_Objects[module].status = I2C_Busy;
+
+  return I2C_Objects[module].status;
 }
 
 /*******************************************************************************
@@ -167,4 +214,15 @@ BaudRate_t SetBaudRate(uint32_t desiredBaudRate)
     }
   }
 }
- 
+
+
+void I2C_IRQHandler(I2C_Module_t module)
+{
+  I2C_CLEAR_IRQ_FLAG;
+
+  if(I2C_GET_TX_MODE)
+  {
+    
+  }
+  else
+}
